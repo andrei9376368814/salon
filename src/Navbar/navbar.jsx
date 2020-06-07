1ec53fa70443c771aeaf4f0c987f18d42cab26e0
Andrey <Andrey9376368814@gmail.com>
2020-06-07 15:49:49 +0300

    добавлена картинка

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -1,28 +1,18 @@
 import React from 'react';
 import './navbar.css';
+import img from '../IMG/vecher_13.jpg';
 
 
 const Navbar=()=>{
     return(
-     <div className="navbar">
-       <nav>
-          <a href = "#">Ссылка 1</a> 
-          <a href = "#">Ссылка 2</a> 
-          <a href = "#">Ссылка 3</a> 
-            <p>Панель навигации</p>
-        </nav>
-        <aside>
-             <p>Справа мы разместили</p>
-        </aside>
-        <section>
-              <h2>Заголовок второго уровня</h2>
-              <p>Раздел 1</p>
-        </section>
-        <section>
-               <h2>Заголовок второго уровня</h2>
-                <p>Раздел 2</p>
-        </section>
-    </div>
+     <div className="table">
+         <div className="cell1">
+             <textarea></textarea>
+         </div>
+         <div className="cell2">
+             <img src={img} alt="cuthair"/>
+         </div>
+     </div>
 
 ) 
 }
