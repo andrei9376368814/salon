418acec5b1b63f214b73e9a8ce52008d784840fc
Andrey <Andrey9376368814@gmail.com>
2020-06-10 07:21:55 +0300

    новая компонента table

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import './navbar.css';
-import img from '../IMG/vecher_13.jpg';
-import Meeting from './meeting';
+import Table from './tables';
+
+
 
 
 const Navbar=()=>{
     return(
-     <div className="table">
-         <div className="cell1">
-             <textarea></textarea>
-         </div>
-         <div className="cell2">
-             <Meeting/>
-             <img src={img} alt="cuthair"/>
-         </div>
-     </div>
-
+    <Table/>
 ) 
 }
 
